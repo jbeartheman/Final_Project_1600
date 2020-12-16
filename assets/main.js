18373885dc086f628c6f1c7b156cc9018f23e8a5
Jordan Powell <bballjordan@gmail.com>
2020-12-15 23:47:14 -0700

    Added a lot fixing and removing alot simplify code

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+const boxGrid = document.querySelector('.certBox col')
+
+
+function createCertBox(singleCertBox)
+let certBox = document.createElement('div')
+certBox.className= 'image fit'
+let certImage = document.createElement('img')
+certImage.src =`../images/`
+
+
+
+
+
+
+loadPage()
